9fac62af712355053e3f60e88b2e13c65fe51c20
Sinantosun <sinan_tosun4141@hotmail.com>
2024-04-16 23:37:32 +0300

    --

--- LeanerProject/Controllers/CoursesController.cs
+++ LeanerProject/Controllers/CoursesController.cs
@@ -17,11 +17,10 @@ namespace LeanerProject.Controllers
             var value = _context.Courses.ToList();
             return View(value);
         }
-        [HttpGet]
-        public ActionResult CourseDetail(int id)
+
+        void dropDowonLoad()
         {
-            var value = _context.Courses.Include(x => x.Category).FirstOrDefault(t => t.CourseId == id);
-            var CategoriesList = _context.Categories.ToList();
+            var CategoriesList = _context.Categories.Where(x => x.Status == true).ToList();
 
             List<SelectListItem> dropDownList = (from x in CategoriesList
                                                  select new SelectListItem
@@ -30,9 +29,14 @@ namespace LeanerProject.Controllers
                                                      Value = x.CategoryId.ToString()
                                                  }).ToList();
             ViewBag.CategoryList = dropDownList;
+        }
 
 
-
+        [HttpGet]
+        public ActionResult CourseDetail(int id)
+        {
+            var value = _context.Courses.Include(x => x.Category).FirstOrDefault(t => t.CourseId == id);
+            dropDowonLoad();
             return View(value);
         }
         [HttpPost]
@@ -45,12 +49,47 @@ namespace LeanerProject.Controllers
             {
                 value.ImageUrl = course.ImageUrl;
             }
-          
+
             value.Price = course.Price;
             value.CategoryId = course.CategoryId;
             _context.SaveChanges();
             TempData["Result"] = "Kayıt güncellendi.";
-            return RedirectToAction("Index");
+            return RedirectToAction("../Courses/Index");
+        }
+
+
+        public ActionResult DeleteCourse(int id)
+        {
+            var value = _context.Courses.Find(id);
+            _context.Courses.Remove(value);
+            _context.SaveChanges();
+            TempData["Result"] = "Kayıt Silindi.";
+            return RedirectToAction("../Courses/Index");
+        }
+
+        [HttpGet]
+        public ActionResult AddCourse()
+        {
+            dropDowonLoad();
+            return View();
+        }
+        [HttpPost]
+        public ActionResult AddCourse(Course course)
+        {
+            _context.Courses.Add(course);
+            _context.SaveChanges();
+            TempData["Result"] = "Kayıt Eklendi";
+            return RedirectToAction("../Courses/Index");
+
+        }
+
+
+        [HttpPost]
+        public ActionResult SearchCourse(string courseName)
+        {
+            var value = _context.Courses.Where(x=>x.CourseName.ToLower().Contains(courseName.ToLower())).ToList();
+            TempData["SearchValue"] = value;
+            return View("Index", value);
         }
     }
 }
---
+++ LeanerProject/Controllers/DashboardController.cs
@@ -0,0 +1,88 @@
+﻿using LeanerProject.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LeanerProject.Controllers
+{
+    public class DashboardController : Controller
+    {
+        Context _context = new Context();
+        public ActionResult Index()
+        {
+            ViewBag.CoursesCount = _context.Courses.Count();
+            ViewBag.CategoryCount = _context.Categories.Where(x => x.Status == true).Count();
+            ViewBag.ClassRoomCount = _context.ClassRooms.Count();
+            ViewBag.RegisteredCount = _context.Students.Count();
+            ViewBag.ExpensiveCourseName = _context.Courses.OrderByDescending(x => x.Price).Select(x => x.CourseName).FirstOrDefault();
+            ViewBag.CheapCourseName = _context.Courses.OrderBy(x => x.Price).Select(y => y.CourseName).FirstOrDefault();
+            ViewBag.CheapCourseName = _context.Courses.OrderBy(x => x.Price).Select(y => y.CourseName).FirstOrDefault();
+            ViewBag.CourseAvgPrice = _context.Courses.Average(y => y.Price);
+
+
+
+
+
+            ViewBag.HightReviewName = _context.Reviews.OrderByDescending(x => x.ReviewValue).Select(y => y.Course.CourseName).FirstOrDefault();
+
+
+            var course1 = _context.Reviews.Where(x => x.Course.Category.CategoryName == "Kodlama").ToList();
+            if (course1.Count != 0)
+            {
+                ViewBag.CourseAvgCoding = course1.Average(y => y.ReviewValue);
+                ViewBag.CourseCodingCount = course1.Count();
+            }
+            else
+            {
+                ViewBag.CourseAvgCoding = 0;
+                ViewBag.CourseCodingCount = "0";
+            }
+
+            var course2 = _context.Reviews.Where(x => x.Course.Category.CategoryName == "Grafik Tasarım").ToList();
+            if (course2.Count != 0)
+            {
+                ViewBag.CourseAvgGraphic = course2.Average(y => y.ReviewValue);
+                ViewBag.CourseGraphicCount = course2.Count();
+            }
+            else
+            {
+                ViewBag.CourseAvgGraphic = 0;
+                ViewBag.CourseGraphicCount = "0";
+            }
+
+            var course3 = _context.Reviews.Where(x => x.Course.Category.CategoryName == "Fotoğrafcılık").ToList();
+            if (course3.Count != 0)
+            {
+                ViewBag.CourseAvgPhoto = course3.Average(y => y.ReviewValue);
+                ViewBag.CoursePhotoCount = course3.Count();
+            }
+            else
+            {
+                ViewBag.CourseAvgPhoto = 0;
+                ViewBag.CoursePhotoCount = "0";
+            }
+
+            var course4 = _context.Reviews.Where(x => x.Course.Category.CategoryName == "İngilizce").ToList();
+            if (course4.Count != 0)
+            {
+                ViewBag.CourseAvgEng = course4.Average(y => y.ReviewValue);
+                ViewBag.CourseEngCount = course4.Count();
+            }
+            else
+            {
+                ViewBag.CourseAvgEng = 0;
+                ViewBag.CourseEngCount = "0";
+            }
+
+
+            ViewBag.EngCount = _context.CourseRegisters.Where(x => x.Course.Category.CategoryName == "İngilizce").Count();
+            ViewBag.CodingCount = _context.CourseRegisters.Where(x => x.Course.Category.CategoryName == "Kodlama").Count();
+            ViewBag.PhotoCount = _context.CourseRegisters.Where(x => x.Course.Category.CategoryName == "Fotoğrafcılık").Count();
+            ViewBag.GraphCount = _context.CourseRegisters.Where(x => x.Course.Category.CategoryName == "Grafik Tasarım").Count();
+            return View();
+        }
+    }
+}
